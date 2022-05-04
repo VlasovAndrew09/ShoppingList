2bd62b5d67adacc38620994443f0df6182812871
381706-1VlasovAndrey <andrio.vlasov.2000@mail.ru>
2022-05-04 13:24:44 +0300

    create ShopListRepositoryImpl and ShopListMapper

---
+++ app/src/main/java/ru/vlasov/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,40 @@
+package ru.vlasov.shoppinglist.data
+
+import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
+import ru.vlasov.shoppinglist.domain.ShopItem
+import ru.vlasov.shoppinglist.domain.ShopListRepository
+import java.lang.RuntimeException
+
+class ShopListRepositoryImpl(
+    application: Application
+) : ShopListRepository {
+
+    private val shopListDao = AppDatabase.getInstance(application).shopListDao()
+    private val mapper = ShopListMapper()
+
+    override suspend fun addShopItem(shopItem: ShopItem) {
+        shopListDao.addShopItem(mapper.mapEntityToDbModel(shopItem))
+    }
+
+    override suspend fun deleteShopItem(shopItem: ShopItem) {
+        shopListDao.deleteShopItem(shopItem.id)
+    }
+
+    override suspend fun editShopItem(shopItem: ShopItem) {
+        shopListDao.addShopItem(mapper.mapEntityToDbModel(shopItem))
+    }
+
+    override suspend fun getShopItem(shopItemId: Int): ShopItem {
+        val dbModel = shopListDao.getShopItem(shopItemId)
+        return mapper.mapDbModelToEntity(dbModel)
+    }
+
+    override fun getShopList(): LiveData<List<ShopItem>> = MediatorLiveData<List<ShopItem>>().apply {
+        addSource(shopListDao.getShopList()) {
+            value = mapper.mapListDbModelToListEntity(it)
+        }
+    }
+}
