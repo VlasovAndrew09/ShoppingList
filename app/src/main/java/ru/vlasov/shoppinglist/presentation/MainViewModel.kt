1dd85b65b0a48b3e9762e521f079a51aea24c134
381706-1VlasovAndrey <andrio.vlasov.2000@mail.ru>
2022-05-04 13:38:46 +0300

    update presentation layer

---
+++ app/src/main/java/ru/vlasov/shoppinglist/presentation/MainViewModel.kt
@@ -0,0 +1,41 @@
+package ru.vlasov.shoppinglist.presentation
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+import ru.vlasov.shoppinglist.data.ShopListRepositoryImpl
+import ru.vlasov.shoppinglist.domain.usecase.DeleteShopItemUseCase
+import ru.vlasov.shoppinglist.domain.usecase.EditShopItemUseCase
+import ru.vlasov.shoppinglist.domain.usecase.GetShopListUseCase
+import ru.vlasov.shoppinglist.domain.ShopItem
+
+
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository = ShopListRepositoryImpl(application)
+
+    private val getShopListUseCase = GetShopListUseCase(repository)
+    private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    val shopList = getShopListUseCase.getShopList()
+
+    fun deleteShopItem(shopItem: ShopItem) {
+        viewModelScope.launch {
+            deleteShopItemUseCase.deleteShopItem(shopItem)
+        }
+    }
+
+    fun changeEnableState(shopItem: ShopItem) {
+        viewModelScope.launch {
+            val newItem = shopItem.copy(enabled = !shopItem.enabled)
+            editShopItemUseCase.editShopItem(newItem)
+        }
+    }
+}
